6c60a7fe0ffdc01916732826d847603c5fb1889a
AndrÃes Manglano <andresmc94@gmail.com>
2016-10-25 12:21:20 +0200

    Change license to MIT

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-        name='extractipy',
-        version='0.0.1',
-        packages=['extractipy'],
-        license='GPL3',
-        long_description='e.e',
-)
